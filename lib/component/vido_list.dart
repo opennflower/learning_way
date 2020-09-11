a12f814da9624951e01fd53869dcc67e17196a32
xz <xz>
2020-09-11 17:25:18 +0800

    注释youTube视频入口

--- lib/component/vido_list.dart
+++ lib/component/vido_list.dart
@@ -1,65 +1,133 @@
 import 'package:flutter/material.dart';
 import 'package:inview_notifier_list/inview_notifier_list.dart';
 import 'package:learning_way/component/text_style.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import 'video_widget.dart';
 
-class VideoList extends StatelessWidget {
+class VideoList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return VideoListView();
+  }
+}
+
+class VideoListView extends State<VideoList> {
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
   @override
   Widget build(BuildContext context) {
     return Stack(
       fit: StackFit.expand,
       children: <Widget>[
-        InViewNotifierList(
-          scrollDirection: Axis.vertical,
-          initialInViewIds: ['0'],
-          isInViewPortCondition:
-              (double deltaTop, double deltaBottom, double viewPortDimension) {
-            return deltaTop < (0.5 * viewPortDimension) &&
-                deltaBottom > (0.5 * viewPortDimension);
-          },
-          itemCount: 10,
-          builder: (BuildContext context, int index) {
-            return Column(
-              children: <Widget>[
-                Container(
-                  height: 44,
-                  width: double.infinity,
-                  child: CNText("头像信息-----"),
-                  color: Color(0xff191154),
-                ),
-                Container(
-                  width: double.infinity,
-                  height: 224.0,
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.only(bottom: 10),
-                  child: LayoutBuilder(
-                    builder: (BuildContext context, BoxConstraints constraints) {
-                      return InViewNotifierWidget(
-                        id: '$index',
-                        builder:
-                            (BuildContext context, bool isInView, Widget child) {
-                          return VideoWidget(
-                              play: isInView,
-                              url:
-                                  'https://vd4.bdstatic.com/mda-icipfkab855n3w0b/mda-icipfkab855n3w0b.mp4?playlist=%5B%22hd%22%2C%22sc%22%5D&auth_key=1597818179-0-0-9eeab1817b3be545c994d9e4cea8ee79&bcevod_channel=searchbox_feed&pd=1&pt=3');
-                        },
-                      );
-                    },
+        SmartRefresher(
+          controller: _refreshController,
+          enablePullDown: true,
+          enablePullUp: true,
+          onRefresh: _onRefresh,
+          onLoading: _onLoading,
+          child: InViewNotifierList(
+            scrollDirection: Axis.vertical,
+            initialInViewIds: ['0'],
+            isInViewPortCondition: (double deltaTop, double deltaBottom,
+                double viewPortDimension) {
+              return deltaTop < (0.5 * viewPortDimension) &&
+                  deltaBottom > (0.5 * viewPortDimension);
+            },
+            itemCount: 10,
+            builder: (BuildContext context, int index) {
+              return Column(
+                children: <Widget>[
+                  Container(
+                    height: 44,
+                    width: double.infinity,
+                    child: CNText("头像信息-----"),
+                    color: Color(0xff191154),
                   ),
-                ),
-              ],
-            );
-          },
+                  Container(
+                    width: double.infinity,
+                    height: 224.0,
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(bottom: 10),
+                    child: LayoutBuilder(
+                      builder:
+                          (BuildContext context, BoxConstraints constraints) {
+                        return InViewNotifierWidget(
+                          id: '$index',
+                          builder: (BuildContext context, bool isInView,
+                              Widget child) {
+                            return VideoWidget(
+                                play: isInView,
+                                url:
+                                    'https://vd4.bdstatic.com/mda-icipfkab855n3w0b/mda-icipfkab855n3w0b.mp4?playlist=%5B%22hd%22%2C%22sc%22%5D&auth_key=1597818179-0-0-9eeab1817b3be545c994d9e4cea8ee79&bcevod_channel=searchbox_feed&pd=1&pt=3');
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
-        // Align(
-        //   alignment: Alignment.center,
-        //   child: Container(
-        //     height: 1.0,
-        //     color: Colors.redAccent,
-        //   ),
-        // )
       ],
     );
   }
+
+  _onRefresh() {
+    _refreshController.refreshCompleted();
+  }
+
+  _onLoading() {
+    _refreshController.loadComplete();
+  }
 }
+
+// return Stack(
+//       fit: StackFit.expand,
+//       children: <Widget>[
+//         InViewNotifierList(
+//           scrollDirection: Axis.vertical,
+//           initialInViewIds: ['0'],
+//           isInViewPortCondition:
+//               (double deltaTop, double deltaBottom, double viewPortDimension) {
+//             return deltaTop < (0.5 * viewPortDimension) &&
+//                 deltaBottom > (0.5 * viewPortDimension);
+//           },
+//           itemCount: 10,
+//           builder: (BuildContext context, int index) {
+//             return Column(
+//               children: <Widget>[
+//                 Container(
+//                   height: 44,
+//                   width: double.infinity,
+//                   child: CNText("头像信息-----"),
+//                   color: Color(0xff191154),
+//                 ),
+//                 Container(
+//                   width: double.infinity,
+//                   height: 224.0,
+//                   alignment: Alignment.center,
+//                   margin: EdgeInsets.only(bottom: 10),
+//                   child: LayoutBuilder(
+//                     builder: (BuildContext context, BoxConstraints constraints) {
+//                       return InViewNotifierWidget(
+//                         id: '$index',
+//                         builder:
+//                             (BuildContext context, bool isInView, Widget child) {
+//                           return VideoWidget(
+//                               play: isInView,
+//                               url:
+//                                   'https://vd4.bdstatic.com/mda-icipfkab855n3w0b/mda-icipfkab855n3w0b.mp4?playlist=%5B%22hd%22%2C%22sc%22%5D&auth_key=1597818179-0-0-9eeab1817b3be545c994d9e4cea8ee79&bcevod_channel=searchbox_feed&pd=1&pt=3');
+//                         },
+//                       );
+//                     },
+//                   ),
+//                 ),
+//               ],
+//             );
+//           },
+//         ),
+
+//       ],
+//     );
--- lib/pages/function/youTube_palyer.dart
+++ lib/pages/function/youTube_palyer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learning_way/component/text_style.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+// import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class YouTuBePlayer extends StatelessWidget {
   @override
@@ -26,99 +26,100 @@ class YouTuBePlayerState extends StatefulWidget {
 }
 
 class YouTuBePlayerView extends State<YouTuBePlayerState> {
-  YoutubePlayerController _controller;
+  // YoutubePlayerController _controller;
 
-  PlayerState _playerState;
-  YoutubeMetaData _videoMetaData;
-  double _volume = 100;
-  bool _muted = false;
-  bool _isPlayerReady = false;
+  // PlayerState _playerState;
+  // YoutubeMetaData _videoMetaData;
+  // double _volume = 100;
+  // bool _muted = false;
+  // bool _isPlayerReady = false;
 
   @override
   void initState() {
     super.initState();
-    _controller = YoutubePlayerController(
-        initialVideoId: 'GrYbMbudILQ',
-        flags: YoutubePlayerFlags(
-          mute: false,
-          autoPlay: true,
-          disableDragSeek: false,
-          loop: false,
-          isLive: false,
-          forceHD: false,
-          enableCaption: true,
-        ))
-      ..addListener(listener);
+    // _controller = YoutubePlayerController(
+    //     initialVideoId: 'GrYbMbudILQ',
+    //     flags: YoutubePlayerFlags(
+    //       mute: false,
+    //       autoPlay: true,
+    //       disableDragSeek: false,
+    //       loop: false,
+    //       isLive: false,
+    //       forceHD: false,
+    //       enableCaption: true,
+    //     ))
+    //   ..addListener(listener);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 240,
-            child: YoutubePlayer(
-              controller: _controller,
-              showVideoProgressIndicator: true,
-              progressIndicatorColor: Colors.blueAccent,
-              topActions: <Widget>[
-                const SizedBox(width: 8.0),
-                Expanded(
-                  child: Text(
-                    _controller.metadata.title,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(
-                    Icons.settings,
-                    color: Colors.white,
-                    size: 25.0,
-                  ),
-                  onPressed: () {
-                    print('Settings Tapped!');
-                  },
-                ),
-              ],
-              onReady: () {
-                _isPlayerReady = true;
-              },
-              onEnded: (data) {
+    return Container();
+    // return Container(
+    //   child: Column(
+    //     children: <Widget>[
+    //       Container(
+    //         width: MediaQuery.of(context).size.width,
+    //         height: 240,
+    //         child: YoutubePlayer(
+    //           controller: _controller,
+    //           showVideoProgressIndicator: true,
+    //           progressIndicatorColor: Colors.blueAccent,
+    //           topActions: <Widget>[
+    //             const SizedBox(width: 8.0),
+    //             Expanded(
+    //               child: Text(
+    //                 _controller.metadata.title,
+    //                 style: const TextStyle(
+    //                   color: Colors.white,
+    //                   fontSize: 18.0,
+    //                 ),
+    //                 overflow: TextOverflow.ellipsis,
+    //                 maxLines: 1,
+    //               ),
+    //             ),
+    //             IconButton(
+    //               icon: const Icon(
+    //                 Icons.settings,
+    //                 color: Colors.white,
+    //                 size: 25.0,
+    //               ),
+    //               onPressed: () {
+    //                 print('Settings Tapped!');
+    //               },
+    //             ),
+    //           ],
+    //           onReady: () {
+    //             _isPlayerReady = true;
+    //           },
+    //           onEnded: (data) {
                
-              },
-            ),
-          )
-        ],
-      ),
-    );
+    //           },
+    //         ),
+    //       )
+    //     ],
+    //   ),
+    // );
   }
 
-  void listener() {
-    if (_isPlayerReady && mounted && !_controller.value.isFullScreen) {
-      setState(() {
-        _playerState = _controller.value.playerState;
-        _videoMetaData = _controller.metadata;
-      });
-    }
-  }
+  // void listener() {
+  //   if (_isPlayerReady && mounted && !_controller.value.isFullScreen) {
+  //     setState(() {
+  //       _playerState = _controller.value.playerState;
+  //       _videoMetaData = _controller.metadata;
+  //     });
+  //   }
+  // }
 
   @override
   void deactivate() {
     // Pauses video while navigating to next page.
-    _controller.pause();
-    super.deactivate();
+    // _controller.pause();
+    // super.deactivate();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
-    super.dispose();
+    // _controller.dispose();
+    // super.dispose();
   }
 }
